2820573c2af5067d51a1c808b583364ef1915ec9
mollyturner <mollyturner50@gmail.com>
2023-02-07 18:41:19 -0600

    Add conditional for changing color of each text area depending on time of day

--- Develop/script.js
+++ Develop/script.js
@@ -1,110 +1,133 @@
-var agendaBtn = document.getElementById('save-button-1');
-var agendaItem = document.getElementById('description-1');
-agendaItem.value = localStorage.getItem('agenda1');
+// Today's date at top of page
+var today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
 
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-1').value;
-
-  localStorage.setItem('agenda1', agendaItem);
-});
-
-var agendaBtn = document.getElementById('save-button-2');
-var agendaItem = document.getElementById('description-2');
-agendaItem.value = localStorage.getItem('agenda2');
-
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-2').value;
-
-  localStorage.setItem('agenda2', agendaItem);
-});
-
-var agendaBtn = document.getElementById('save-button-3');
-var agendaItem = document.getElementById('description-3');
-agendaItem.value = localStorage.getItem('agenda3');
-
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-3').value;
-
-  localStorage.setItem('agenda3', agendaItem);
-});
-
-var agendaBtn = document.getElementById('save-button-4');
-var agendaItem = document.getElementById('description-4');
-agendaItem.value = localStorage.getItem('agenda4');
-
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-4').value;
-
-  localStorage.setItem('agenda4', agendaItem);
-});
-
-var agendaBtn = document.getElementById('save-button-5');
-var agendaItem = document.getElementById('description-5');
-agendaItem.value = localStorage.getItem('agenda5');
-
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-5').value;
-
-  localStorage.setItem('agenda5', agendaItem);
-});
-
-var agendaBtn = document.getElementById('save-button-6');
-var agendaItem = document.getElementById('description-6');
-agendaItem.value = localStorage.getItem('agenda6');
-
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-6').value;
-
-  localStorage.setItem('agenda6', agendaItem);
-});
-
-var agendaBtn = document.getElementById('save-button-7');
-var agendaItem = document.getElementById('description-7');
-agendaItem.value = localStorage.getItem('agenda7');
-
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-7').value;
-
-  localStorage.setItem('agenda7', agendaItem);
-});
-
-var agendaBtn = document.getElementById('save-button-8');
-var agendaItem = document.getElementById('description-8');
-agendaItem.value = localStorage.getItem('agenda8');
-
-agendaBtn.addEventListener('click', function () {
-  var agendaItem = document.getElementById('description-8').value;
-
-  localStorage.setItem('agenda8', agendaItem);
-});
-
+// Adds event listener for each click button and saves to local storage
 var agendaBtn = document.getElementById('save-button-9');
 var agendaItem = document.getElementById('description-9');
-agendaItem.value = localStorage.getItem('agenda9');
+agendaItem.value = localStorage.getItem('agenda-9');
 
 agendaBtn.addEventListener('click', function () {
   var agendaItem = document.getElementById('description-9').value;
 
-  localStorage.setItem('agenda9', agendaItem);
+  localStorage.setItem('agenda-9', agendaItem);
 });
 
+var agendaBtn = document.getElementById('save-button-10');
+var agendaItem = document.getElementById('description-10');
+agendaItem.value = localStorage.getItem('agenda-10');
 
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-10').value;
 
-$(function () {
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
-  var today = dayjs();
-  $('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
-
+  localStorage.setItem('agenda-10', agendaItem);
 });
+
+var agendaBtn = document.getElementById('save-button-11');
+var agendaItem = document.getElementById('description-11');
+agendaItem.value = localStorage.getItem('agenda-11');
+
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-11').value;
+
+  localStorage.setItem('agenda-11', agendaItem);
+});
+
+var agendaBtn = document.getElementById('save-button-12');
+var agendaItem = document.getElementById('description-12');
+agendaItem.value = localStorage.getItem('agenda-12');
+
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-12').value;
+
+  localStorage.setItem('agenda-12', agendaItem);
+});
+
+var agendaBtn = document.getElementById('save-button-1');
+var agendaItem = document.getElementById('description-1');
+agendaItem.value = localStorage.getItem('agenda-1');
+
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-1').value;
+
+  localStorage.setItem('agenda-1', agendaItem);
+});
+
+var agendaBtn = document.getElementById('save-button-2');
+var agendaItem = document.getElementById('description-2');
+agendaItem.value = localStorage.getItem('agenda-2');
+
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-2').value;
+
+  localStorage.setItem('agenda-2', agendaItem);
+});
+
+var agendaBtn = document.getElementById('save-button-3');
+var agendaItem = document.getElementById('description-3');
+agendaItem.value = localStorage.getItem('agenda-3');
+
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-3').value;
+
+  localStorage.setItem('agenda-3', agendaItem);
+});
+
+var agendaBtn = document.getElementById('save-button-4');
+var agendaItem = document.getElementById('description-4');
+agendaItem.value = localStorage.getItem('agenda-4');
+
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-4').value;
+
+  localStorage.setItem('agenda-4', agendaItem);
+});
+
+var agendaBtn = document.getElementById('save-button-5');
+var agendaItem = document.getElementById('description-5');
+agendaItem.value = localStorage.getItem('agenda-5');
+
+agendaBtn.addEventListener('click', function () {
+  var agendaItem = document.getElementById('description-5').value;
+
+  localStorage.setItem('agenda-5', agendaItem);
+});
+
+// Conditional for adding a class to each text area depending on time of day 
+var currentTime = dayjs().hour();
+
+function timeBlockColor() {
+
+  for (let index = 0; index < 9; index++) {
+    var timeBlock = 0;
+
+    // Makes timeBlock run in 12H format 
+    if (index < 4) {
+      timeBlock = index + 9;
+    } else if (index >= 4) {
+      timeBlock = index - 3;
+    }
+
+    var classNam = ['past', 'present', 'future'];
+    var elem = document.getElementById(`hour-${timeBlock}`);
+
+    // Restarts timeBlock back to 24H time format
+    if (index >= 4) {
+      timeBlock = (timeBlock + 12);
+    }
+
+    // Uses 24H format for checking and adding the correct time class to change each hour display
+    if (currentTime < timeBlock) {
+      elem.classList.add(classNam[2]);
+    } else if (currentTime == timeBlock) {
+      elem.classList.add(classNam[1]);
+    } else if (currentTime > timeBlock) {
+      elem.classList.add(classNam[0]);
+    };
+  }
+};
+
+timeBlockColor();
+
+
+
